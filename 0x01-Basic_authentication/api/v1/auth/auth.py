c0669f6363f5e20e819a07b82bc9205bd7e9bc71
Ayobami <oludemiayobami@gmail.com>
2024-05-21 23:03:53 +0100

    feat(task 4): added task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -30,7 +30,14 @@ class Auth:
         Returns:
             bool: False (placeholder implementation).
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for p in excluded_paths:
+            if p.startswith(path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
