870a9197cb7a3be885c843aaef7f3d424f187478
Ayobami <oludemiayobami@gmail.com>
2024-05-26 16:23:12 +0100

    fix(filter_loger): checker fixes

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,17 +7,26 @@ import logging
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """Constructor function
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Formats a log record
+
+            Args:
+                record: the record to format
+            Returns:
+                a string representing the formatted record
+        """
         record.msg = filter_datum(fields=self.fields,
                                   message=record.getMessage(),
                                   redaction=self.REDACTION,
