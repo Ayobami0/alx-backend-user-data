46242fc38d4e32dc32a988c284f9b63e6cbeb766
Ayobami <oludemiayobami@gmail.com>
2024-05-26 16:16:38 +0100

    fix(filter_loger): checker fixes

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 """0. Regex-ing"""
-
-import re
 from typing import List
+import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(fields=self.fields,
+                                  message=record.getMessage(),
+                                  redaction=self.REDACTION,
+                                  separator=self.SEPARATOR,)
+        return super().format(record)
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -17,6 +37,7 @@ def filter_datum(fields: List[str], redaction: str,
         Returns:
             The obfuscated messages seperated by {seperator}
     """
-    return ';'.join([re.sub(r'(?<={}\=).*'.format(v.split('=')[0]), redaction,
-                            v) if v.split('=')[0] in fields else v
-                     for v in message.split(separator)])
+    for v in message.split(separator):
+        if v.split('=')[0] in fields:
+            message = re.sub("{}".format(v.split("=")[1]), redaction, message)
+    return message
